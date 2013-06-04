85f4c2fdce3788882156bde541907b33b91b6603
Kenny Song <jellyksong@gmail.com>
2013-06-04 13:24:13 -0400

    channel API working

--- main.py
+++ main.py
@@ -1,25 +1,82 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import jinja2
+import logging
+import os
+import random
+import string
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+from google.appengine.ext import db
+from google.appengine.api import channel
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+class Rooms(db.Model):
+	message = db.StringProperty()
+	room_id = db.StringProperty()
+	client_ids = db.StringListProperty()
+
+class BaseHandler(webapp2.RequestHandler):
+	'''Parent class for all handlers'''
+	def write(self, content):
+		return self.response.out.write(content)
+
+	def rget(self, name):
+		'''Gets a HTTP parameter'''
+		return self.request.get(name)
+
+	def render(self, template, params={}):
+		template = jinja_env.get_template(template)
+		self.response.out.write(template.render(params))
+
+	def set_cookie(self, cookie):
+		self.response.headers.add_header('Set-Cookie', cookie)
+
+	def delete_cookie(self, cookie):
+		self.response.headers.add_header('Set-Cookie', '%s=; Path=/' % cookie)
+
+class MainHandler(BaseHandler):
+    def get(self):
+    	client_id = rand_str(10)
+    	token = channel.create_channel(client_id)
+    	room_id = self.rget('room_id')
+    	if not room_id: 
+    		# create new room
+    		room_id = rand_str(5)
+    		room = Rooms(room_id=room_id, client_ids=[client_id])
+    		room.put()
+    	else:
+			# add user to existing room
+			room = get_by_room_id(room_id)
+			room.client_ids.append(client_id)
+			room.put()
+
+        self.render('index.html', {'client_id':client_id, 'room_id':room_id, 'token':token})
+
+class UpdateHandler(BaseHandler):
+	def post(self):
+		room_id = self.rget('room_id')
+		message = self.rget('message')
+
+		# change on db
+		room = get_by_room_id(room_id)
+		room.message = message
+		room.put()
+
+		# push to clients
+		for client_id in room.client_ids:
+			channel.send_message(client_id, message)
+
+
+def rand_str(n):
+	return ''.join(random.choice(string.ascii_lowercase) for x in range(n))
+
+def get_by_room_id(room_id):
+	q = Rooms.all()
+	q.filter('room_id =', room_id)
+	return q.get()
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+							   ('/update', UpdateHandler),
+							  ], debug=True)
